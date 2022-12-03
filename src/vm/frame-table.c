ca8972f4ee75520579c7ba294a874de6889a7191
solesie <tkddbs9801@gmail.com>
2022-12-03 09:33:25 +0000

    feat: add data structure for memory sharing

---
+++ src/vm/frame-table.c
@@ -0,0 +1,383 @@
+/* 4.1.5) Unless you have implemented sharing, only a single page 
+   should refer to a frame at any given time.
+   
+   when multiple processes are created that use the
+   same executable file, share read-only pages among those processes instead of
+   creating separate copies of read-only segments for each process.
+   
+   mmap 사용시에만 sharing을 구현한다. 한 frame이 여러 user_page를 저장할 수 있도록 한다.
+   즉, hash에 중복을 허용하여 bucket에 저장하는 함수들을 구현한다.
+   frame.c:struct hash frame_table만 이 함수들을 사용하도록 한다. */
+
+//#include "lib/kernel/hash.h"
+#include "lib/debug.h"
+#include "threads/malloc.h"
+#include "vm/frame-table.h"
+#include "lib/kernel/list.h"
+
+#define list_elem_to_hash_elem(LIST_ELEM)                       \
+        list_entry(LIST_ELEM, struct vm_ft_hash_elem, list_elem)
+
+static struct list *find_bucket (struct vm_ft_hash *, struct vm_ft_hash_elem *);
+static struct vm_ft_same_keys* find_elem (struct vm_ft_hash *, struct list *,
+                                    struct vm_ft_hash_elem *);
+static void insert_elem (struct vm_ft_hash *, struct list *, struct vm_ft_hash_elem *);
+static struct list_elem * remove_elem (struct vm_ft_hash *, struct vm_ft_hash_elem *);
+static void rehash (struct vm_ft_hash *);
+
+/* Initializes hash table H to compute hash values using HASH and
+   compare hash elements using LESS, given auxiliary data AUX. */
+bool
+vm_ft_hash_init (struct vm_ft_hash *h,
+           vm_ft_hash_hash_func *hash, vm_ft_hash_less_func *less, void *aux) 
+{
+  h->elem_cnt = 0;
+  h->bucket_cnt = 4;
+  h->buckets = malloc (sizeof *h->buckets * h->bucket_cnt);
+  h->hash = hash;
+  h->less = less;
+  h->aux = aux;
+
+  if (h->buckets != NULL) 
+    {
+      vm_ft_hash_clear (h, NULL);
+      return true;
+    }
+  else
+    return false;
+}
+
+/* Removes all the elements from H.
+   
+   If DESTRUCTOR is non-null, then it is called for each element
+   in the hash.  DESTRUCTOR may, if appropriate, deallocate the
+   memory used by the hash element.  However, modifying hash
+   table H while hash_clear() is running, using any of the
+   functions hash_clear(), hash_destroy(), hash_insert(),
+   hash_replace(), or hash_delete(), yields undefined behavior,
+   whether done in DESTRUCTOR or elsewhere. */
+void
+vm_ft_hash_clear (struct vm_ft_hash *h, vm_ft_hash_action_func *destructor) 
+{
+  size_t i;
+
+  for (i = 0; i < h->bucket_cnt; i++) 
+    {
+      struct list *bucket = &h->buckets[i];
+
+      if (destructor != NULL) 
+        while (!list_empty (bucket)) 
+          {
+            struct list_elem *list_elem = list_pop_front (bucket);
+            struct vm_ft_hash_elem *hash_elem = list_elem_to_hash_elem (list_elem);
+            destructor (hash_elem, h->aux);
+          }
+
+      list_init (bucket); 
+    }    
+
+  h->elem_cnt = 0;
+}
+
+/* Destroys hash table H.
+
+   If DESTRUCTOR is non-null, then it is first called for each
+   element in the hash.  DESTRUCTOR may, if appropriate,
+   deallocate the memory used by the hash element.  However,
+   modifying hash table H while hash_clear() is running, using
+   any of the functions hash_clear(), hash_destroy(),
+   hash_insert(), hash_replace(), or hash_delete(), yields
+   undefined behavior, whether done in DESTRUCTOR or
+   elsewhere. */
+void
+vm_ft_hash_destroy (struct vm_ft_hash *h, vm_ft_hash_action_func *destructor) 
+{
+  if (destructor != NULL)
+    vm_ft_hash_clear (h, destructor);
+  free (h->buckets);
+}
+
+/* Inserts NEW into hash table H and returns a null pointer. */   
+void
+vm_ft_hash_insert (struct vm_ft_hash *h, struct vm_ft_hash_elem *new)
+{
+  struct list *bucket = find_bucket (h, new);
+
+  insert_elem (h, bucket, new);
+
+  rehash (h);
+}
+
+
+/* Finds and returns "list of elements" equal to E in hash table H, or a
+   null pointer if no equal element exists in the table. */
+struct vm_ft_same_keys*
+vm_ft_hash_find (struct vm_ft_hash *h, struct vm_ft_hash_elem *e) 
+{
+  return find_elem (h, find_bucket (h, e), e);
+}
+
+
+/* PintOs list library 구조상 이미 buckets에 리스트에서 또 다른 list를 추출하는 것이 매우 까다로웠다.
+   buckets의 vm_ft_hash_elem을 가르키는 배열을 나타내는 구조체를 반환하도록 하였고,
+   따라서 그 배열을 삭제해주어야 한다. */
+void vm_ft_same_keys_free(struct vm_ft_same_keys* arr){
+  free(arr->pointers_arr_of_ft_hash_elem);
+}
+
+
+/* Finds, removes, and returns "list of elements" equal to E in hash
+   table H.  Returns a null pointer if no equal element existed
+   in the table.
+   
+   동일한 e를 전부다 제거한다. */
+struct vm_ft_same_keys*
+vm_ft_hash_delete (struct vm_ft_hash *h, struct vm_ft_hash_elem *e)
+{
+  struct vm_ft_same_keys* founds = find_elem(h, find_bucket(h,e), e);
+  if(founds == NULL)
+    return NULL;
+  
+  for (int i = 0; i < founds->len; ++i)
+    remove_elem (h, founds->pointers_arr_of_ft_hash_elem[i]);
+  
+  rehash (h); 
+
+  return founds;
+}
+
+
+/* Initializes I for iterating hash table H.
+
+   Iteration idiom:
+
+      struct hash_iterator i;
+
+      hash_first (&i, h);
+      while (hash_next (&i))
+        {
+          struct foo *f = vm_ft_hash_entry (hash_cur (&i), struct foo, elem);
+          ...do something with f...
+        }
+
+   Modifying hash table H during iteration, using any of the
+   functions hash_clear(), hash_destroy(), hash_insert(),
+   hash_replace(), or hash_delete(), invalidates all
+   iterators. */
+void
+vm_ft_hash_first (struct vm_ft_hash_iterator *i, struct vm_ft_hash *h) 
+{
+  ASSERT (i != NULL);
+  ASSERT (h != NULL);
+
+  i->hash = h;
+  i->bucket = i->hash->buckets;
+  i->elem = list_elem_to_hash_elem (list_head (i->bucket));
+}
+
+/* Advances I to the next element in the hash table and returns
+   it.  Returns a null pointer if no elements are left.  Elements
+   are returned in arbitrary order.
+
+   Modifying a hash table H during iteration, using any of the
+   functions hash_clear(), hash_destroy(), hash_insert(),
+   hash_replace(), or hash_delete(), invalidates all
+   iterators. */
+struct vm_ft_hash_elem *
+vm_ft_hash_next (struct vm_ft_hash_iterator *i)
+{
+  ASSERT (i != NULL);
+
+  i->elem = list_elem_to_hash_elem (list_next (&i->elem->list_elem));
+  while (i->elem == list_elem_to_hash_elem (list_end (i->bucket)))
+    {
+      if (++i->bucket >= i->hash->buckets + i->hash->bucket_cnt)
+        {
+          i->elem = NULL;
+          break;
+        }
+      i->elem = list_elem_to_hash_elem (list_begin (i->bucket));
+    }
+  
+  return i->elem;
+}
+
+/* Returns the current element in the hash table iteration, or a
+   null pointer at the end of the table.  Undefined behavior
+   after calling hash_first() but before hash_next(). */
+struct vm_ft_hash_elem *
+vm_ft_hash_cur (struct vm_ft_hash_iterator *i) 
+{
+  return i->elem;
+}
+
+/* Returns the number of elements in H. */
+size_t
+vm_ft_hash_size (struct vm_ft_hash *h) 
+{
+  return h->elem_cnt;
+}
+
+/* Returns true if H contains no elements, false otherwise. */
+bool
+vm_ft_hash_empty (struct vm_ft_hash *h) 
+{
+  return h->elem_cnt == 0;
+}
+
+
+/* Returns a hash of integer I. */
+unsigned
+vm_ft_hash_int (int i) 
+{
+  return hash_bytes (&i, sizeof i);
+}
+
+
+/* Returns the bucket in H that E belongs in. */
+static struct list *
+find_bucket (struct vm_ft_hash *h, struct vm_ft_hash_elem *e) 
+{
+  size_t bucket_idx = h->hash (e, h->aux) & (h->bucket_cnt - 1);
+  return &h->buckets[bucket_idx];
+}
+
+
+/* Searches BUCKET in H for a hash element equal to E.  Returns
+   it if found or a null pointer otherwise. */
+//static struct hash_elem *
+static struct vm_ft_same_keys*
+find_elem (struct vm_ft_hash *h, struct list *bucket, struct vm_ft_hash_elem *e) 
+{
+  struct list_elem *i;
+
+  int same = 0;
+
+  for (i = list_begin (bucket); i != list_end (bucket); i = list_next (i)) 
+    {
+      struct vm_ft_hash_elem *hi = list_elem_to_hash_elem (i);
+      if (!h->less (hi, e, h->aux) && !h->less (e, hi, h->aux))
+        ++same;
+    }
+  
+  if(same == 0) return NULL;
+  
+  struct vm_ft_same_keys* ret = malloc(sizeof(struct vm_ft_same_keys));
+  ret->len = same;
+  ret->pointers_arr_of_ft_hash_elem = malloc(sizeof(struct vm_ft_hash_elem*) * same);
+
+  int count = 0;
+  for (i = list_begin (bucket); i != list_end (bucket); i = list_next (i), ++count) 
+    {
+      struct vm_ft_hash_elem *hi = list_elem_to_hash_elem (i);
+      if (!h->less (hi, e, h->aux) && !h->less (e, hi, h->aux)){
+        ret->pointers_arr_of_ft_hash_elem[count] = hi;
+      }
+    }
+  
+  return ret;
+}
+
+/* Returns X with its lowest-order bit set to 1 turned off. */
+static inline size_t
+turn_off_least_1bit (size_t x) 
+{
+  return x & (x - 1);
+}
+
+/* Returns true if X is a power of 2, otherwise false. */
+static inline size_t
+is_power_of_2 (size_t x) 
+{
+  return x != 0 && turn_off_least_1bit (x) == 0;
+}
+
+/* Element per bucket ratios. */
+#define MIN_ELEMS_PER_BUCKET  1 /* Elems/bucket < 1: reduce # of buckets. */
+#define BEST_ELEMS_PER_BUCKET 2 /* Ideal elems/bucket. */
+#define MAX_ELEMS_PER_BUCKET  4 /* Elems/bucket > 4: increase # of buckets. */
+
+/* Changes the number of buckets in hash table H to match the
+   ideal.  This function can fail because of an out-of-memory
+   condition, but that'll just make hash accesses less efficient;
+   we can still continue. */
+static void
+rehash (struct vm_ft_hash *h) 
+{
+  size_t old_bucket_cnt, new_bucket_cnt;
+  struct list *new_buckets, *old_buckets;
+  size_t i;
+
+  ASSERT (h != NULL);
+
+  /* Save old bucket info for later use. */
+  old_buckets = h->buckets;
+  old_bucket_cnt = h->bucket_cnt;
+
+  /* Calculate the number of buckets to use now.
+     We want one bucket for about every BEST_ELEMS_PER_BUCKET.
+     We must have at least four buckets, and the number of
+     buckets must be a power of 2. */
+  new_bucket_cnt = h->elem_cnt / BEST_ELEMS_PER_BUCKET;
+  if (new_bucket_cnt < 4)
+    new_bucket_cnt = 4;
+  while (!is_power_of_2 (new_bucket_cnt))
+    new_bucket_cnt = turn_off_least_1bit (new_bucket_cnt);
+
+  /* Don't do anything if the bucket count wouldn't change. */
+  if (new_bucket_cnt == old_bucket_cnt)
+    return;
+
+  /* Allocate new buckets and initialize them as empty. */
+  new_buckets = malloc (sizeof *new_buckets * new_bucket_cnt);
+  if (new_buckets == NULL) 
+    {
+      /* Allocation failed.  This means that use of the hash table will
+         be less efficient.  However, it is still usable, so
+         there's no reason for it to be an error. */
+      return;
+    }
+  for (i = 0; i < new_bucket_cnt; i++) 
+    list_init (&new_buckets[i]);
+
+  /* Install new bucket info. */
+  h->buckets = new_buckets;
+  h->bucket_cnt = new_bucket_cnt;
+
+  /* Move each old element into the appropriate new bucket. */
+  for (i = 0; i < old_bucket_cnt; i++) 
+    {
+      struct list *old_bucket;
+      struct list_elem *elem, *next;
+
+      old_bucket = &old_buckets[i];
+      for (elem = list_begin (old_bucket);
+           elem != list_end (old_bucket); elem = next) 
+        {
+          struct list *new_bucket
+            = find_bucket (h, list_elem_to_hash_elem (elem));
+          next = list_next (elem);
+          list_remove (elem);
+          list_push_front (new_bucket, elem);
+        }
+    }
+
+  free (old_buckets);
+}
+
+/* Inserts E into BUCKET (in hash table H). */
+static void
+insert_elem (struct vm_ft_hash *h, struct list *bucket, struct vm_ft_hash_elem *e) 
+{
+  h->elem_cnt++;
+  list_push_front (bucket, &e->list_elem);
+}
+
+/* Removes E from hash table H. */
+static struct list_elem *
+remove_elem (struct vm_ft_hash *h, struct vm_ft_hash_elem *e) 
+{
+  h->elem_cnt--;
+  list_remove (&e->list_elem);
+}
+
---
+++ src/vm/frame-table.h
@@ -0,0 +1,106 @@
+#ifndef __VM_FRAME_TABLE
+#define __VM_FRAME_TABLE
+
+/* Hash table.
+
+   This data structure is thoroughly documented in the Tour of
+   Pintos for Project 3.
+
+   This is a standard hash table with chaining.  To locate an
+   element in the table, we compute a hash function over the
+   element's data and use that as an index into an array of
+   doubly linked lists, then linearly search the list.
+
+   The chain lists do not use dynamic allocation.  Instead, each
+   structure that can potentially be in a hash must embed a
+   struct vm_ft_hash_elemmember.  All of the hash functions operate on
+   these `struct hash_elem's.  The hash_entry macro allows
+   conversion from a struct vm_ft_hash_elemback to a structure object
+   that contains it.  This is the same technique used in the
+   linked list implementation.  Refer to lib/kernel/list.h for a
+   detailed explanation. */
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include "list.h"
+
+/* Hash element. */
+struct vm_ft_hash_elem
+  {
+    struct list_elem list_elem;
+  };
+
+/* Converts pointer to hash element HASH_ELEM into a pointer to
+   the structure that HASH_ELEM is embedded inside.  Supply the
+   name of the outer structure STRUCT and the member name MEMBER
+   of the hash element.  See the big comment at the top of the
+   file for an example. */
+#define vm_ft_hash_entry(HASH_ELEM, STRUCT, MEMBER)                   \
+        ((STRUCT *) ((uint8_t *) &(HASH_ELEM)->list_elem              \
+                     - offsetof (STRUCT, MEMBER.list_elem)))
+
+/* Computes and returns the hash value for hash element E, given
+   auxiliary data AUX. */
+typedef unsigned vm_ft_hash_hash_func (const struct vm_ft_hash_elem*e, void *aux);
+
+/* Compares the value of two hash elements A and B, given
+   auxiliary data AUX.  Returns true if A is less than B, or
+   false if A is greater than or equal to B. */
+typedef bool vm_ft_hash_less_func (const struct vm_ft_hash_elem*a,
+                             const struct vm_ft_hash_elem*b,
+                             void *aux);
+
+/* Performs some operation on hash element E, given auxiliary
+   data AUX. */
+typedef void vm_ft_hash_action_func (struct vm_ft_hash_elem*e, void *aux);
+
+/* Hash table. */
+struct vm_ft_hash 
+  {
+    size_t elem_cnt;            /* Number of elements in table. */
+    size_t bucket_cnt;          /* Number of buckets, a power of 2. */
+    struct list *buckets;       /* Array of `bucket_cnt' lists. */
+    vm_ft_hash_hash_func *hash;       /* Hash function. */
+    vm_ft_hash_less_func *less;       /* Comparison function. */
+    void *aux;                  /* Auxiliary data for `hash' and `less'. */
+  };
+
+/* A hash table iterator. */
+struct vm_ft_hash_iterator 
+  {
+    struct vm_ft_hash *hash;          /* The hash table. */
+    struct list *bucket;        /* Current bucket. */
+    struct vm_ft_hash_elem*elem;     /* Current hash element in current bucket. */
+  };
+
+struct vm_ft_same_keys{
+  struct vm_ft_hash_elem** pointers_arr_of_ft_hash_elem;
+  int len;
+};
+
+/* Basic life cycle. */
+bool vm_ft_hash_init (struct vm_ft_hash *, vm_ft_hash_hash_func *, vm_ft_hash_less_func *, void *aux);
+void vm_ft_hash_clear (struct vm_ft_hash *, vm_ft_hash_action_func *);
+void vm_ft_hash_destroy (struct vm_ft_hash *, vm_ft_hash_action_func *);
+
+void vm_ft_same_keys_free(struct vm_ft_same_keys* arr);
+
+/* Search, insertion, deletion. */
+void vm_ft_hash_insert (struct vm_ft_hash *, struct vm_ft_hash_elem*);
+struct vm_ft_same_keys* vm_ft_hash_find (struct vm_ft_hash *, struct vm_ft_hash_elem*);
+struct vm_ft_same_keys* vm_ft_hash_delete (struct vm_ft_hash *, struct vm_ft_hash_elem*);
+
+/* Iteration. */
+void vm_ft_hash_first (struct vm_ft_hash_iterator *, struct vm_ft_hash *);
+struct vm_ft_hash_elem* vm_ft_hash_next (struct vm_ft_hash_iterator *);
+struct vm_ft_hash_elem* vm_ft_hash_cur (struct vm_ft_hash_iterator *);
+
+/* Information. */
+size_t vm_ft_hash_size (struct vm_ft_hash *);
+bool vm_ft_hash_empty (struct vm_ft_hash *);
+
+/* Sample hash functions. */
+unsigned vm_ft_hash_int (int);
+
+#endif /* vm/frame-table.h */
